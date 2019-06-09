9f77cbe2d2988d9840037606ed53a3daebda0e55
xfgu <632167902@qq.com>
2019-06-09 12:32:22 +0800

    增加了layout布局

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,58 @@
 import React from 'react';
-import logo from '../logo.svg';
+import {Tabs, WhiteSpace} from 'antd-mobile';
+import {StickyContainer, Sticky} from 'react-sticky';
 import '../styles/App.css';
 
-function App() {
+function renderTabBar(props) {
+  return (<Sticky>
+    {({style}) => <div style={{...style, zIndex: 1}}><Tabs.DefaultTabBar {...props} /></div>}
+  </Sticky>);
+}
+
+const tabs = [
+  {title: 'First Tab'},
+  {title: 'Second Tab'},
+  {title: 'Third Tab'},
+];
+const App = () => {
   return (
     <div className="App">
-      11
+      <WhiteSpace/>
+      <StickyContainer>
+        <Tabs tabs={tabs}
+              initalPage={'t2'}
+              renderTabBar={renderTabBar}
+        >
+          <div style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '250px',
+            backgroundColor: '#fff'
+          }}>
+            Content of first tab
+          </div>
+          <div style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '250px',
+            backgroundColor: '#fff'
+          }}>
+            Content of second tab
+          </div>
+          <div style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '250px',
+            backgroundColor: '#fff'
+          }}>
+            Content of third tab
+          </div>
+        </Tabs>
+      </StickyContainer>
+      <WhiteSpace/>
     </div>
   );
 }
---
+++ src/layout/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      34
+    </div>
+  );
+};
+
+export default Header;
