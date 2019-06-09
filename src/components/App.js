46b9cc486ab32c045e9d22167cbf615ffcb37e47
xfgu <632167902@qq.com>
2019-06-09 12:01:52 +0800

    增加了ant

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../logo.svg';
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      11
+    </div>
+  );
+}
+
+export default App;
